780486dde9ccc8f486f7eae6919806a86cf9dcc4
Ravid Levy <ravidlevy99@gmail.com>
2021-02-21 01:08:55 +0200

    Handled Combo Boxes

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -3,10 +3,6 @@ from tkinter.ttk import *
 from controller import Controller
 from tkinter import filedialog
 
-
-
-
-
 BUTTON_WIDTH = 20
 COMBO_BOX_WIDTH = 20
 BROWSE_WIDTH = 10
@@ -23,7 +19,35 @@ TITLE = 'Automated Microscope'
 PLACEHOLDER = 'Set Directory'
 
 
+def focus_out_entry_box(widget, widget_text):
+    if widget['foreground'] != 'Grey' and len(widget.get()) == 0:
+        widget.delete(0, 'end')
+        widget['foreground'] = 'Grey'
+        widget.insert(0, widget_text)
 
+
+def focus_in_entry_box(widget):
+    if widget['foreground'] != 'Black':
+        widget['foreground'] = 'Black'
+        if widget.get() == PLACEHOLDER:
+            widget.delete(0, 'end')
+
+
+def create_menu():
+    root = Tk()
+    root.title(TITLE)
+    root.geometry(WINDOW_DIMENSIONS)
+    label = Label(root, text=TITLE, font=('Times', TITLE_FONT_SIZE))
+
+    style = Style()
+    style.configure('TButton', font=('Times', FONT_SIZE),
+                    borderwidth='1')
+    style.map('TButton', foreground=[('active', 'black')],
+              background=[('active', 'white')])
+
+    label.pack()
+    menu = Label(root)
+    return menu, root
 
 
 class GUI:
@@ -31,32 +55,23 @@ class GUI:
 
         self.controller = Controller()
 
-        self.menu, self.root = self.create_menu()
+        self.menu, self.root = create_menu()
         self.execute_button = self.create_execute(self.menu)
         self.set_directory_button, self.directory_path_label = self.create_set_directory(self.menu)
-        self.choose_problem_domain, self.choose_microsope, self.choose_event_detector = self.create_combo_boxes(self.menu)
+        self.choose_problem_domain, self.choose_microscope, self.choose_event_detector =\
+            self.create_combo_boxes(self.menu)
         self.create_action_button = self.create_action_configuration(self.menu)
 
         self.menu.pack(side=LEFT)
 
-    def create_menu(self):
-        root = Tk()
-        root.title(TITLE)
-        root.geometry(WINDOW_DIMENSIONS)
-        label = Label(root, text=TITLE, font=('Times', TITLE_FONT_SIZE))
+    def set_problem_domain(self, event):
+        choice = event.widget.current()
+        microscopes = self.controller.set_problem_domain(choice)
+        self.choose_microscope['values'] = microscopes
 
-        style = Style()
-        style.configure('TButton', font=('Times', FONT_SIZE),
-                        borderwidth='1')
-        style.map('TButton', foreground=[('active', 'black')],
-                  background=[('active', 'white')])
-
-        label.pack()
-        menu = Label(root)
-        return menu, root
-
-
-
+    def set_microscope(self, event):
+        choice = event.widget.current()
+        self.controller.set_microscope(choice)
 
     def set_event_detector(self, event):
         choice = event.widget.current()
@@ -72,15 +87,16 @@ class GUI:
         path = filedialog.askdirectory(initialdir="/", title="Select a directory")
         self.directory_path_label.delete(0, 'end')
         self.directory_path_label.insert(0, path)
+        self.directory_path_label['foreground'] = 'Black'
         self.controller.set_image_path(path)
-
+        print(self.directory_path_label.get())
 
     def create_set_directory(self, menu):
         set_directory = Label(menu)
         path = Entry(set_directory, font=('Times', FONT_SIZE), foreground='Grey')
         path.insert(0, PLACEHOLDER)
-        path.bind("<FocusIn>", lambda args: self.focus_in_entry_box(path))
-        path.bind("<FocusOut>", lambda args: self.focus_out_entry_box(path, PLACEHOLDER))
+        path.bind("<FocusIn>", lambda args: focus_in_entry_box(path))
+        path.bind("<FocusOut>", lambda args: focus_out_entry_box(path, PLACEHOLDER))
         path.place(width=PATH_WIDTH, height=PATH_HEIGHT)
         path.pack(side=LEFT)
 
@@ -90,18 +106,19 @@ class GUI:
         set_directory.pack(padx=SPACEX, pady=SPACEY)
         return browse, path
 
-
     def create_combo_boxes(self, menu):
         choose_problem_domain = Combobox(menu, state="readonly", width=COMBO_BOX_WIDTH, font=('Times', FONT_SIZE))
         choose_problem_domain.option_add('*TCombobox*Listbox.font', ('Times', FONT_SIZE))
         choose_problem_domain.set("Choose Problem Domain")
         choose_problem_domain.pack(padx=SPACEX, pady=SPACEY)
+        choose_problem_domain.bind("<<ComboboxSelected>>", self.set_problem_domain)
+        choose_problem_domain['values'] = self.controller.problem_domains
 
         choose_microscope = Combobox(menu, state="readonly", width=COMBO_BOX_WIDTH, font=('Times', FONT_SIZE))
         choose_microscope.option_add('*TCombobox*Listbox.font', ('Times', FONT_SIZE))
         choose_microscope.set("Choose Microscope")
+        choose_microscope.bind("<<ComboboxSelected>>", self.set_microscope)
         choose_microscope.pack(padx=SPACEX, pady=SPACEY)
-        choose_microscope['values'] = ["AVI"]
 
         choose_event_detector = Combobox(menu, state="readonly", width=COMBO_BOX_WIDTH, font=('Times', FONT_SIZE))
         choose_event_detector.option_add('*TCombobox*Listbox.font', ('Times', FONT_SIZE))
@@ -112,28 +129,14 @@ class GUI:
 
         return choose_problem_domain, choose_microscope, choose_event_detector
 
-
+    def open_action_configuration(self):
+        pass
 
     def create_action_configuration(self, menu):
-        action_configuration = Button(menu, text="Action Configuration")
+        action_configuration = Button(menu, text="Action Configuration", command=self.open_action_configuration)
         action_configuration.config(width=BUTTON_WIDTH)
         action_configuration.pack(padx=SPACEX, pady=SPACEY)
         return action_configuration
-
-
-    def focus_out_entry_box(self, widget, widget_text):
-        if widget['foreground'] != 'Grey' and len(widget.get()) == 0:
-            widget.delete(0, 'end')
-            widget['foreground'] = 'Grey'
-            widget.insert(0, widget_text)
-
-
-    def focus_in_entry_box(self, widget):
-        if widget['foreground'] != 'Black':
-            widget['foreground'] = 'Black'
-            widget.delete(0, 'end')
-
-
 
     def run(self):
         self.root.mainloop()
--- controller.py
+++ controller.py
@@ -1,13 +1,6 @@
 
 import os
 import sys
-import pathlib
-
-
-
-parent_dir = os.path.split(os.getcwd())[0]
-sys.path.extend([x[0] for x in os.walk(parent_dir) if '.git' not in x[0]])
-
 from entities.event_detector import EventDetector
 from logs.log_setup import setup_loggers
 from ed_adapters.ed_adapter_mock import EDAdapterMock
@@ -16,16 +9,14 @@ from definitions import global_vars, VARNAMES
 import logging
 from threading import Thread
 
-executing = False
+parent_dir = os.path.split(os.getcwd())[0]
+sys.path.extend([x[0] for x in os.walk(parent_dir) if '.git' not in x[0]])
 
+executing = False
 
 setup_loggers()
 info_logger = logging.getLogger('info')
 error_logger = logging.getLogger('exceptions')
-
-
-
-
 
 
 def check_if_running(func):
@@ -36,13 +27,18 @@ def check_if_running(func):
             return func(*args, **kwargs)
     return wrap
 
+
 class Controller:
     def __init__(self):
         self.am_adapter = None
         self.ed_adapter = None
+        self.problem_domain = None
+        self.microscope = None
         self.detector_path = None
         self.image_path = None
-        self.problem_domains = []
+        self.problem_domains = ["Cell Fusion - Fly Spit"]
+        self.domain_microscopes = {"Cell Fusion - Fly Spit": ["AVI"]}
+        self.microscopes = {"AVI": None}
         self.detectors = []
 
     @check_if_running
@@ -52,6 +48,15 @@ class Controller:
                 if os.path.isdir(os.path.join(global_vars[VARNAMES.ed_path.value], name)):
                     self.detectors.append(EventDetector(os.path.join(global_vars[VARNAMES.ed_path.value], name)))
         return self.detectors
+
+    @check_if_running
+    def set_problem_domain(self, index):
+        self.problem_domain = self.problem_domains[index]
+        return self.domain_microscopes[self.problem_domain]
+
+    @check_if_running
+    def set_microscope(self, index):
+        self.microscope = self.domain_microscopes[self.problem_domain][index]
 
     @check_if_running
     def set_detector(self, index):
@@ -76,6 +81,3 @@ class Controller:
     def stop(self):
         self.am_adapter.stop()
         self.ed_adapter.stop()
-
-
-
