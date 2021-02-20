46752dc503bc4a568513f6f3e25f8e8eb04b196e
vkoukoul <viktor.koukouliev@intel.com>
2021-02-20 21:28:20 +0200

    first commit

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.ttk import *
 from controller import Controller
+from tkinter import filedialog
 
 
 
@@ -27,18 +28,48 @@ PLACEHOLDER = 'Set Directory'
 
 class GUI:
     def __init__(self):
+
         self.controller = Controller()
 
-    def set_event_detector (self, event):
+        self.menu, self.root = self.create_menu()
+        self.execute_button = self.create_execute(self.menu)
+        self.set_directory_button, self.directory_path_label = self.create_set_directory(self.menu)
+        self.choose_problem_domain, self.choose_microsope, self.choose_event_detector = self.create_combo_boxes(self.menu)
+        self.create_action_button = self.create_action_configuration(self.menu)
+
+        self.menu.pack(side=LEFT)
+
+    def create_menu(self):
+        root = Tk()
+        root.title(TITLE)
+        root.geometry(WINDOW_DIMENSIONS)
+        label = Label(root, text=TITLE, font=('Times', TITLE_FONT_SIZE))
+
+        style = Style()
+        style.configure('TButton', font=('Times', FONT_SIZE),
+                        borderwidth='1')
+        style.map('TButton', foreground=[('active', 'black')],
+                  background=[('active', 'white')])
+
+        label.pack()
+        menu = Label(root)
+        return menu, root
+
+    def set_event_detector(self, event):
         choice = event.widget.current()
         self.controller.set_detector(choice)
-
 
     def create_execute(self, menu):
         execute_button = Button(menu, text="Execute", command=self.controller.run)
         execute_button.config(width=BUTTON_WIDTH)
         execute_button.pack(padx=SPACEX, pady=SPACEY)
+        return execute_button
 
+    def browse_files(self):
+        path = filedialog.askdirectory(initialdir="/", title="Select a directory")
+        self.directory_path_label.delete(0, 'end')
+        self.directory_path_label.insert(0, path)
+        self.controller.set_image_path(path)
 
 
     def create_set_directory(self, menu):
@@ -50,10 +81,11 @@ class GUI:
         path.place(width=PATH_WIDTH, height=PATH_HEIGHT)
         path.pack(side=LEFT)
 
-        browse = Button(set_directory, text="Browse")
+        browse = Button(set_directory, text="Browse", command=self.browse_files)
         browse.config(width=BROWSE_WIDTH)
         browse.pack()
         set_directory.pack(padx=SPACEX, pady=SPACEY)
+        return browse, path
 
 
     def create_combo_boxes(self, menu):
@@ -75,12 +107,15 @@ class GUI:
         choose_event_detector.bind("<<ComboboxSelected>>", self.set_event_detector)
         choose_event_detector['values'] = [x.name for x in self.controller.get_detectors()]
 
+        return choose_problem_domain, choose_microscope, choose_event_detector
+
 
 
     def create_action_configuration(self, menu):
         action_configuration = Button(menu, text="Action Configuration")
         action_configuration.config(width=BUTTON_WIDTH)
         action_configuration.pack(padx=SPACEX, pady=SPACEY)
+        return action_configuration
 
 
     def focus_out_entry_box(self, widget, widget_text):
@@ -96,29 +131,10 @@ class GUI:
             widget.delete(0, 'end')
 
 
-    def main(self):
-        root = Tk()
-        root.title(TITLE)
-        root.geometry(WINDOW_DIMENSIONS)
-        label = Label(root, text=TITLE, font=('Times', TITLE_FONT_SIZE))
 
-        style = Style()
-        style.configure('TButton', font=('Times', FONT_SIZE),
-                        borderwidth='1')
-        style.map('TButton', foreground=[('active', 'black')],
-                  background=[('active', 'white')])
-
-        label.pack()
-        menu = Label(root)
-
-        self.create_execute(menu)
-        self.create_set_directory(menu)
-        self.create_combo_boxes(menu)
-        self.create_action_configuration(menu)
-
-        menu.pack(side=LEFT)
-        root.mainloop()
+    def run(self):
+        self.root.mainloop()
 
 
 if __name__ == '__main__':
-    GUI().main()
+    GUI().run()
