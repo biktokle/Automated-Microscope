209ff127018cd1c2811700c83a1ad3c6c0b61273
Viktor_Koukouliev <viktor_koukouliev@dell.com>
2021-05-19 21:57:16 +0300

    adding tests

---
+++ tests/unit/test_controller.py
@@ -0,0 +1,35 @@
+import importlib
+import unittest
+from unittest import TestCase
+from unittest import mock
+from unittest.mock import patch, MagicMock
+from undecorated import undecorated
+
+from am_adapters.am_adapter_avi import AMAdapterAVI
+from controller.controller import Controller
+
+
+class TestController(TestCase):
+
+    def setUp(self):
+        self.controller = Controller()
+        self.controller.create_adapters = MagicMock(return_value=(MagicMock(), MagicMock()))
+
+    def test_stop_when_running(self):
+        self.controller.executing = True
+        self.controller.ed_adapter = MagicMock()
+        self.controller.ed_adapter.stop = MagicMock()
+        self.controller.stop()
+        assert self.controller.ed_adapter.stop.called
+
+    def test_stop_when_not_running(self):
+        self.controller.ed_adapter = MagicMock()
+        self.controller.ed_adapter.stop = MagicMock()
+        self.controller.stop()
+        assert not self.controller.ed_adapter.stop.called
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
