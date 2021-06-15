8dbad517091b4b90f8b25c472024711417792d0f
Viktor_Koukouliev <viktor_koukouliev@dell.com>
2021-06-15 14:24:38 +0300

    remove image from detectors

--- entities/event_detector.py
+++ entities/event_detector.py
@@ -13,14 +13,13 @@ class EventDetector:
         """
         self.path = path
         self.name = os.path.basename(path)
-        self.detector_path, self.image, self.description = self.get_data()
+        self.detector_path, self.description = self.get_data()
 
     def get_data(self):
         """
         :return: the file path, a sample image and a description of the event detector.
         """
         detector_path = None
-        image = None
         description = ""
         for name in os.listdir(self.path):
             suffix = pathlib.Path(os.path.join(self.path, name)).suffix
@@ -33,10 +32,5 @@ class EventDetector:
                     description = f.read()
             elif suffix == '.py':
                 detector_path = full_path
-            else:
-                try:
-                    image = MicroscopeImage(full_path)
-                except Exception as e:
-                    pass
 
-        return detector_path, image, description
+        return detector_path, description
